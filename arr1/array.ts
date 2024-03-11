730b5de242a2fa72abc960a47b37dc04f69e3822
usama <ahtishamrajputchohan@gmail.com>
2024-03-11 10:20:05 +0500

    add file array.ts

---
+++ arr1/array.ts
@@ -0,0 +1,11 @@
+let frnds = ['esa', 'bina', 2, 45, true, false];
+console.log (frnds);
+
+let fry:string [ ] = ['bina', 'rida', 'hina'];
+console.log(fry);
+
+fry[1] ='tina';
+console.log(fry);
+
+console.log (frnds[2] ,frnds[4]);
+
