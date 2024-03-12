0e6e1fde267af64a7771b8976d16ec48417e8b7d
muhammad usama 00275369 <ahtishamrajputchohan@gmail.com>
2024-03-12 15:37:57 +0500

    add object.ts

---
+++ arr1/array.js
@@ -0,0 +1,8 @@
+"use strict";
+let frnds = ['esa', 'bina', 2, 45, true, false];
+console.log(frnds);
+let fry = ['bina', 'rida', 'hina'];
+console.log(fry);
+fry[1] = 'tina';
+console.log(fry);
+console.log(frnds[2], frnds[4]);
---
+++ arr1/object.js
@@ -0,0 +1,16 @@
+var city = {
+    name: {
+        currentName: "Karachi",
+        previous: "Kolachi",
+    },
+    population: 2500000,
+    famousPlaces: {
+        historicalPlaces: ['Mazar ghazi', 'jinnah residency'],
+        foodPoints: ['burns road', 'nazimabad'],
+        picnicPlaces: ['seaview', 'clifton', 'meusum'],
+    },
+};
+console.log(city);
+console.log(city.famousPlaces.foodPoints);
+console.log(city.name.currentName);
+console.log(city.famousPlaces.picnicPlaces[0]);
